6520ed1f27a216dec0fdf5884c2338bede9a6f70
Qussai Ramzi <qussairamzi@live.com>
2026-08-09 04:56:19 +0100

    Added coffee dictionary

--- main.py
+++ main.py
@@ -16,5 +16,48 @@ def main():
     choice = get_user_choice()
     print(f"You chose: {choice}")
 
+coffees = [
+    {
+        "name": "Mina",
+        "origin": "Brazil",
+        "process": "Natural",
+        "roast": "Medium",
+    },
+    {
+        "name": "Huila",
+        "origin": "Colombia",
+        "process": "Washed",
+        "roast": "Medium",
+    },
+
+    {
+        "name": "Inzuzi",
+        "origin": "Rwanda",
+        "process": "Natural",
+        "roast": "Medium",
+    },
+
+    {
+        "name": "Aquia",
+        "origin": "Costa Rica",
+        "process": "Natural",
+        "roast": "Medium",
+    },
+
+    {
+        "name": "Armonia",
+        "origin": "Guatemala",
+        "process": "Washed",
+        "roast": "Medium",
+    },
+    {
+        "name": "Gibuzale",
+        "origin": "Uganda",
+        "process": "Washed",
+        "roast": "Medium",
+    },
+    
+]
+
 
 main()
